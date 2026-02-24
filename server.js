e434fea3c235e3826bd6d60aaecde179706a28f3
Moksha Mary s <25bcac40@kristujayanti.com>
2026-02-24 21:32:45 +0530

    Serve frontend on Render

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
+const path = require("path");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/contact", (req, res) => {
-  const data = req.body;
-
-  fs.appendFileSync("contacts.txt", JSON.stringify(data) + "\n");
-
-  console.log("New Contact:", data);
-
-  res.send("Data received successfully");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-console.log("TEST LINE");
